b44ce1d94722225846658017edc3d114bb89ff97
Jhonatan Ospina <jhonatan.ospina@utp.edu.co>
2020-05-04 11:14:19 -0500

    Autómatas celulares

--- Cellular Automata for Physical Modelling/cell.js
+++ Cellular Automata for Physical Modelling/cell.js
@@ -1,18 +1,13 @@
 class Cell {
-    constructor(posX, posY, size) {
+    constructor(posX, posY, size, pos) {
         this.neighbours = []
-        this.color = color(random(50,255))
         this.posX = posX
         this.posY = posY
         this.size = size
-        this.material = "inert"
-    }
-
-    isInert() {
-        if (this.material = "inert")
-            return true
-        else
-            return false
+        this.alive = 0
+        this.aliveNeighbours = 0
+        this.color = color(255)
+        this.pos = pos
     }
 
     addNeighbour(neighbour) {
@@ -23,16 +18,44 @@ class Cell {
         this.color = newColor
     }
 
+    update(cells) {
+        this.aliveNeighbours = 0
+        for (let i = 0; i < cells[this.pos].neighbours.length; i++) {
+            if(cells[this.pos].neighbours[i] != undefined) {
+                if(cells[this.pos].neighbours[i].alive == 1) {
+                    this.aliveNeighbours += 1
+                }
+            } 
+        }
+        if (this.alive == 0) {
+            if (this.aliveNeighbours == 3) {
+                this.alive = 1
+            }
+        }
+        if (this.alive == 1) {
+            if (this.aliveNeighbours <= 2) {
+                this.alive = 0
+            }
+            if (this.aliveNeighbours >= 4) {
+                this.alive = 0
+            }
+        }
+    } 
+
     showNeighbours() {
         for (let i = 0; i < this.neighbours.length; i++) {
             if(this.neighbours[i] != undefined) {
                 this.neighbours[i].changeColor(color(0,255,0))
-            }
-            
+            } 
         }
     }
 
     draw() {
+        if(this.alive == 0) {
+            this.color = color(255)
+        } else {
+            this.color = color(0)
+        }
         fill(this.color)
         rect(this.posX, this.posY, this.size, this.size)
     }
--- Cellular Automata for Physical Modelling/sketch.js
+++ Cellular Automata for Physical Modelling/sketch.js
@@ -1,13 +1,18 @@
 let cells = []
+let newCells = []
 let size = 800
 let gridValue = 32
 let cutSize = size/gridValue
+let button;
 
 function setup() {
-    createCanvas(size, size)
+    createCanvas(size+1, size+1)
+    button = createButton('click me');
+    button.position(19, 820);
+    button.mousePressed(newStep);
     for (let i = 0; i < cutSize; i++) {
         for (let j = 0; j < cutSize; j++) {
-            cells[i + j*cutSize] = new Cell(i*gridValue, j*gridValue,gridValue/2)
+            cells[i + j*cutSize] = new Cell(i*gridValue, j*gridValue,gridValue,i + j*cutSize)
         }
     }
     for (let i = 0; i < cutSize; i++) {
@@ -23,11 +28,34 @@ function setup() {
             cells[i + j*cutSize].addNeighbour(cells[(i-1) + (j-1)*cutSize])
         }
     }
-    cells[1].showNeighbours()
+    newCells = cells
+    noLoop()
 }
 
 function draw() {
+    frameRate(5)
     background(0)
+    for (let i = 0; i < cutSize; i++) {
+        for (let j = 0; j < cutSize; j++) {
+            cells[i + j*cutSize].update(newCells)
+        }
+    }
+    for (let i = 0; i < cutSize; i++) {
+        for (let j = 0; j < cutSize; j++) {
+            cells[i + j*cutSize].draw()
+        }
+    }
+    newCells = cells
+}
+
+function newStep() {
+    draw()
+}
+
+function mousePressed() {
+    print(floor(mouseX/size * cutSize))
+    print(mouseY)
+    cells[floor(mouseX/width * cutSize) + floor(mouseY/height * cutSize)*cutSize].alive = 1
     for (let i = 0; i < cutSize; i++) {
         for (let j = 0; j < cutSize; j++) {
             cells[i + j*cutSize].draw()
